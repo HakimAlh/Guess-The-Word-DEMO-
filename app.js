66f2fd81ecf28d6f4e009ae1957847bc45343683
Hakim <hakimalhaddad2@gmail.com>
2025-01-19 15:55:09 +0300

    Tower-Terror: Demo [Update 1]

---
+++ app.js
@@ -0,0 +1,33 @@
+// /*-------------------------------- Constants --------------------------------*/
+let words = []
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+const letterElement = document.querySelector('.Letters-Rows')
+const inputElement = document.querySelector('.Inputs')
+const inputBtn = document.querySelector('.Input')
+const btnElement = document.querySelector('#btn')
+/*-------------------------------- Functions --------------------------------*/
+const wordRandomizer = () => {
+const wordsRandom = Math.floor(Math.random() * 3)
+wordsRandom = words
+console.log(wordRandomizer)
+}
+
+function btnFunction () {
+    if (btnElement =
+    return inputBtn.textContent = 'A'
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+inputBtn.addEventListener('click', btnFunction)
+
+/*----------------------------- SCRAPPED CODE -----------------------------*/
+// const words = ['Tower','Drawing','Terror','Block']
+// const words = [{Word:'Tower' , letter: T: ,'T' }] 
+// function letterElement (){
+//     if letterElement === 
+//     }
+// function words(){
+    
+// }
