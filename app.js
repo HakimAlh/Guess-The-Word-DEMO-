1ce2cce3be2ac3cadf71840a2cb4546543c8b257
Hakim <hakimalhaddad2@gmail.com>
2025-01-19 16:33:34 +0300

    Tower-Terror: Demo [Update 2]

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const letterElement = document.querySelector('.Letters-Rows')
 const inputElement = document.querySelector('.Inputs')
 const inputBtn = document.querySelector('.Input')
 const btnElement = document.querySelector('#btn')
+const wordElement = document.querySelector('.Words')
+// const word1Element = document.querySelector('.Towers')
 /*-------------------------------- Functions --------------------------------*/
 const wordRandomizer = () => {
 const wordsRandom = Math.floor(Math.random() * 3)
@@ -14,13 +16,20 @@ wordsRandom = words
 console.log(wordRandomizer)
 }
 
-function btnFunction () {
-    if (btnElement =
-    return inputBtn.textContent = 'A'
-}
+// function btnFunction = () => {
+// (btnElement = 'click')
+// if (btnElement === word1Element)
+// return 
+
+
+// }
+const wordFound = document.querySelector(".Tower");
+wordFound.addEventListener("click",
+    () => (wordFound.textContent = 'test')
+)
 
 /*----------------------------- Event Listeners -----------------------------*/
-inputBtn.addEventListener('click', btnFunction)
+// inputBtn.addEventListener('click', )
 
 /*----------------------------- SCRAPPED CODE -----------------------------*/
 // const words = ['Tower','Drawing','Terror','Block']
@@ -30,4 +39,9 @@ inputBtn.addEventListener('click', btnFunction)
 //     }
 // function words(){
     
+// }
+
+// function btnFunction () {
+//     if (btnElement =
+//     return inputBtn.textContent = 'A'
 // }
