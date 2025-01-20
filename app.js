519c3efdd54795561b0e4acaddcd5be48d0234d6
Hakim <hakimalhaddad2@gmail.com>
2025-01-20 15:56:27 +0300

    Tower-Terror: Demo [Update 3.1]

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
-// let words = []
+
 /*-------------------------------- Variables --------------------------------*/
-const words = ['Tower','Terror']
+const word = [{words: 'Tower', char:[5]} ,
+{words: 'Terror', char:[6]}
+]
+console.log(word)
 /*------------------------ Cached Element References ------------------------*/
 const letterElement = document.querySelector('#Letters-Rows')
 const btnElement = document.querySelectorAll('.Letters')
 console.log(btnElement)
 /*-------------------------------- Functions --------------------------------*/
-const wordRandomizer = () => {
-const wordsRandom = Math.floor(Math.random() * 3)
+const wordRandomizer = (word) => {
+word = Math.floor(Math.random())
 wordsRandom = words
 console.log(wordRandomizer)
 }
@@ -19,12 +22,12 @@ console.log('Am I clicking?')
  } 
 }
 
-// words = Math.floor(Math.random())
-// words.ForEach(word) => {
-//     const inputElement = document.createElement('input'); {
-// inputElement.textContent = '_'
-//     } }
-
+word.words.forEach(word() => {
+    (if word.char === ))
+return  document.createElement('input') {
+    input.textContent = '_'
+    
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 letterElement.addEventListener('click', clickedBtn)
@@ -50,3 +53,11 @@ letterElement.addEventListener('click', clickedBtn)
 
 
 // }
+
+// let words = []
+
+// words = Math.floor(Math.random())
+// words.ForEach(word) => {
+//     const inputElement = document.createElement('input'); {
+// inputElement.textContent = '_'
+//     } }
